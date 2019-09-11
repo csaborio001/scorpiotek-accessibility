f2857c3d8504b19b026714138cb588d3ff659c14
csaborio <christian@saborio.org>
2019-09-11 13:07:23 +1000

    Added fonts folder, referenced the CSS from assets.php, and referenced the fonts with the class inside the CSS

--- inc/assets.php
+++ inc/assets.php
@@ -1,3 +1,19 @@
 <?php
 
 // Load the CSS and the JavaScript File in this file
+
+define('accessibility_version', 1);
+
+function accessibility_enqueue_scripts($hook){
+    
+    wp_enqueue_script( ('scorpiotek-accessibility'), 
+    jsPath, 
+        array('jquery'), 
+        null, true );
+
+    // You must load the CSS the same way as you load the JS.
+    wp_register_style( 'accessibility_css', cssPath );
+    wp_enqueue_style( 'accessibility_css' );
+}
+
+ add_action( 'wp_enqueue_scripts', 'accessibility_enqueue_scripts' );
